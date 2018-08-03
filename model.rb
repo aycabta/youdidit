79cdf9b9218a64bed412f8d2356b75d5d2b1dadb
aycabta <aycabta@gmail.com>
2018-08-04 00:34:47 +0900

    Use JSON like Hash literal

--- model.rb
+++ model.rb
@@ -7,10 +7,10 @@ require 'open-uri'
 class User
   include DataMapper::Resource
   property :id, Serial
-  property :user_id, Decimal, :precision => 20, :required => true
-  property :email, String, :length => 256, :required => true
-  property :name, String, :length => 256, :required => true
-  property :nickname, String, :length => 256, :required => true
+  property :user_id, Decimal, precision: 20, required: true
+  property :email, String, length: 256, required: true
+  property :name, String, length: 256, required: true
+  property :nickname, String, length: 256, required: true
 end
 
 DataMapper.finalize
