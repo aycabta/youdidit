03c60e6b65e1bd235ce4efdd79a02208ba2aa219
aycabta <aycabta@gmail.com>
2018-08-03 22:28:50 +0900

    Add model.rb

---
+++ model.rb
@@ -0,0 +1,20 @@
+require 'bundler'
+require 'dm-core'
+require 'dm-migrations'
+require 'tempfile'
+require 'open-uri'
+
+class User
+  include DataMapper::Resource
+  property :id, Serial
+  property :user_id, Decimal, :precision => 20, :required => true
+  property :email, String, :length => 256, :required => true
+  property :name, String, :length => 256, :required => true
+  property :nickname, String, :length => 256, :required => true
+end
+
+DataMapper.finalize
+
+def database_upgrade!
+  User.auto_upgrade!
+end
